61167ee99959f9362361e467f09138d53472259f
Simon H <github@pointbeing.net>
2019-01-26 17:48:09 +0000

    Add all files for first bootstrap exercise

--- labs/lab2/bootstrap.py
+++ labs/lab2/bootstrap.py
@@ -5,8 +5,24 @@ import seaborn as sns
 import numpy as np
 
 
-def boostrap(sample, sample_size, iterations):
-	# <---INSERT YOUR CODE HERE--->
+def bootstrap(sample, sample_size, iterations, confidence_interval=95):
+
+	# Create an array of samples of shape (iterations, sample_size)
+	new_samples = np.random.choice(sample, size=(iterations, sample_size))
+
+	# Calculate and save the mean of the array (we return it at the end)
+	data_mean = new_samples.mean()
+
+	# In each iteration
+	# - calculate the mean of the iteration data
+	# - store it in an array
+	means = np.array([iteration.mean() for iteration in new_samples])
+
+	# Calculate the lower and upper bounds for a given CI
+	gap = (100 - confidence_interval) / 2
+	lower = np.percentile(means, gap)
+	upper = np.percentile(means, 100 - gap)
+
 	return data_mean, lower, upper
 
 
@@ -15,8 +31,11 @@ if __name__ == "__main__":
 
 	data = df.values.T[1]
 	boots = []
+
+	#boot = bootstrap(data, data.shape[0], 10)
+
 	for i in range(100, 100000, 1000):
-		boot = boostrap(data, data.shape[0], i)
+		boot = bootstrap(data, data.shape[0], i)
 		boots.append([i, boot[0], "mean"])
 		boots.append([i, boot[1], "lower"])
 		boots.append([i, boot[2], "upper"])
@@ -33,7 +52,3 @@ if __name__ == "__main__":
 
 	#print ("Mean: %f")%(np.mean(data))
 	#print ("Var: %f")%(np.var(data))
-	
-
-
-	
